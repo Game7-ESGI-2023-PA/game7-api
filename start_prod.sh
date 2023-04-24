df9c19e461b65926e4c39c0d799adf147fe3ef70
saziz <shan.azizh@gmail.com>
2023-04-24 13:38:02 +0200

    devops: clean and deploy

--- start_prod.sh
+++ start_prod.sh
@@ -5,13 +5,6 @@ if [ $# -ne 2 ]; then
     exit 1
 fi
 
-if [ -d "game7-api" ]; then
-    cd game7-api || return
-    git pull
-else
-    git clone git@github.com:Game7-ESGI-2023-PA/game7-api.git game7-api
-fi
-
 export APP_SECRET=$1
 export DATABASE_URL=$2
 export CORS_ALLOW_ORIGIN='*'
