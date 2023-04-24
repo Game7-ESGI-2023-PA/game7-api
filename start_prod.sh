7e6fe52a2e299859a9cda17695f2dcc22db78c84
saziz <shan.azizh@gmail.com>
2023-04-24 14:14:04 +0200

    devops: test

--- start_prod.sh
+++ start_prod.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]; then
-    echo "Error: Please provide APP_SECRET, DATABASE_URL and GIT_SSH_PASSPHRASE a as parameters."
+if [ $# -ne 2 ]; then
+    echo "Error: Please provide APP_SECRET, DATABASE_URL a as parameters."
     exit 1
-fi
-
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_ed25519 -p "$3"
-
-if [ -d "game7-api" ]; then
-    cd game7-api || return
-    git pull
-else
-    git clone git@github.com:Game7-ESGI-2023-PA/game7-api.git game7-api
 fi
 
 export APP_SECRET=$1
