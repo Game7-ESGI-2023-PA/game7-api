4c3f9f34ef9bafaba7eaadb34e5e1aecaeb08104
saziz <shan.azizh@gmail.com>
2023-04-24 16:01:05 +0200

    devops: test

--- start_prod.sh
+++ start_prod.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-    echo "Error: Please provide APP_SECRET, DATABASE_URL a as parameters."
+if [ $# -ne 3 ]; then
+    echo "Error: Please provide APP_SECRET, DATABASE_URL SERVER_NAME a as parameters."
     exit 1
 fi
 
 export APP_SECRET=$1
 export DATABASE_URL=$2
+export SERVER_NAME=$3
 export CORS_ALLOW_ORIGIN='*'
 
 docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d
