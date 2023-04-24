d7b851360c05f880cc46cd5ee7972e06342f2929
saziz <shan.azizh@gmail.com>
2023-04-24 16:29:25 +0200

    devops: test

--- start_prod.sh
+++ start_prod.sh
@@ -5,6 +5,13 @@ if [ $# -ne 3 ]; then
     exit 1
 fi
 
+if [ -d 'game7-api' ]; then
+	cd game7-api && git pull
+else
+	git clone git@github.com:Game7-ESGI-2023-PA/game7-api.git
+	cd game7-api
+fi
+
 export APP_SECRET=$1
 export DATABASE_URL=$2
 export SERVER_NAME=$3
