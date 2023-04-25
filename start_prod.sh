aeb3581817e64bed0a1568054e4999069801e35b
saziz <shan.azizh@gmail.com>
2023-04-25 18:10:37 +0200

    devops: edit .env

--- start_prod.sh
+++ start_prod.sh
@@ -23,7 +23,7 @@ else
     cd game7-api || return
 fi
 
-sudo APP_SECRET="$APP_SECRET" DATABASE_URL="$DATABASE_URL" CORS_ALLOW_ORIGIN='*' docker compose -f docker-compose.yml -f docker-compose.prod.yml up php caddy -d --build
+sudo APP_SECRET="$APP_SECRET" DATABASE_URL="$DATABASE_URL" CORS_ALLOW_ORIGIN='*' docker compose -f docker-compose.yml -f docker-compose.prod.yml up php caddy -d
 
 
 exit_status=$?
