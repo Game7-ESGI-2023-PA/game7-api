6ff261c0c6c65070203170926fe19e4b40963613
saziz <shan.azizh@gmail.com>
2023-04-25 20:01:18 +0200

    feat: auto manage jwt sign

--- start_prod.sh
+++ start_prod.sh
@@ -7,9 +7,12 @@ fi
 
 APP_SECRET_NAME="game7-app-secret"
 DATABASE_URL_NAME="game7-dburl"
+JWT_PASSPHRASE_NAME="game7-jwt-passphrase"
 
 APP_SECRET=$(aws secretsmanager get-secret-value --secret-id "$APP_SECRET_NAME" --query 'SecretString' --output text)
 DATABASE_URL=$(aws secretsmanager get-secret-value --secret-id "$DATABASE_URL_NAME" --query 'SecretString' --output text)
+JWT_PASSPHRASE=$(aws secretsmanager get-secret-value --secret-id "$JWT_PASSPHRASE_NAME" --query 'SecretString' --output text)
+
 
 if [ -z "$APP_SECRET" ] || [ -z "$DATABASE_URL" ]; then
     echo "Error: Failed to fetch APP_SECRET and/or DATABASE_URL from AWS Secrets Manager."
@@ -28,6 +31,9 @@ APP_ENV='prod' \
 APP_SECRET="$APP_SECRET" \
 DATABASE_URL="$DATABASE_URL" \
 CORS_ALLOW_ORIGIN='*' \
+JWT_PRIVATE_KEY_PATH=%kernel.project_dir%/config/jwt/private.pem \
+JWT_PUBLIC_KEY_PATH=%kernel.project_dir%/config/jwt/private.pem \
+JWT_PASSPHRASE="$JWT_PASSPHRASE"
 docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build
 
 
