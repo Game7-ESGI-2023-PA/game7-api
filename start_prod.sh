849fbf4820782b633f1b6cfea7e9d92a186395fe
saziz <shan.azizh@gmail.com>
2023-04-25 17:40:34 +0200

    devops: test

--- start_prod.sh
+++ start_prod.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-#if [ -z "$(which aws)" ]; then
-#    echo "Error: AWS CLI not found. Please install and configure AWS CLI."
-#    exit 1
-#fi
-#
-#APP_SECRET_NAME="game7-app-secret"
-#DATABASE_URL_NAME="game7-dburl"
-#
-#APP_SECRET=$(aws secretsmanager get-secret-value --secret-id "$APP_SECRET_NAME" --query 'SecretString' --output text)
-#DATABASE_URL=$(aws secretsmanager get-secret-value --secret-id "$DATABASE_URL_NAME" --query 'SecretString' --output text)
-#
-#if [ -z "$APP_SECRET" ] || [ -z "$DATABASE_URL" ]; then
-#    echo "Error: Failed to fetch APP_SECRET and/or DATABASE_URL from AWS Secrets Manager."
-#    exit 1
-#fi
+if [ -z "$(which aws)" ]; then
+    echo "Error: AWS CLI not found. Please install and configure AWS CLI."
+    exit 1
+fi
+
+APP_SECRET_NAME="game7-app-secret"
+DATABASE_URL_NAME="game7-dburl"
+
+APP_SECRET=$(aws secretsmanager get-secret-value --secret-id "$APP_SECRET_NAME" --query 'SecretString' --output text)
+DATABASE_URL=$(aws secretsmanager get-secret-value --secret-id "$DATABASE_URL_NAME" --query 'SecretString' --output text)
+
+if [ -z "$APP_SECRET" ] || [ -z "$DATABASE_URL" ]; then
+    echo "Error: Failed to fetch APP_SECRET and/or DATABASE_URL from AWS Secrets Manager."
+    exit 1
+fi
 
 if [ -d 'game7-api' ]; then
     cd game7-api && git pull
@@ -23,9 +23,9 @@ else
     cd game7-api || return
 fi
 
-#export APP_SECRET="$APP_SECRET"
-#export DATABASE_URL="$DATABASE_URL"
-#export CORS_ALLOW_ORIGIN='*'
+export APP_SECRET="$APP_SECRET"
+export DATABASE_URL="$DATABASE_URL"
+export CORS_ALLOW_ORIGIN='*'
 sudo docker compose -f docker-compose.yml -f docker-compose.prod.yml up php caddy -d --build
 
 exit_status=$?
