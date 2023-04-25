da7929507693fb2832fa0bdcadcb16096e4ddd1a
saziz <shan.azizh@gmail.com>
2023-04-25 14:23:25 +0200

    devops: get env from aws

--- start_prod.sh
+++ start_prod.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
-if [ -d 'game7-api' ]; then
-	cd game7-api && git pull
-else
-	git clone git@github.com:Game7-ESGI-2023-PA/game7-api.git
-	cd game7-api || return
+if [ -z "$(which aws)" ]; then
+    echo "Error: AWS CLI not found. Please install and configure AWS CLI."
+    exit 1
 fi
 
-sudo docker compose -f docker-compose.yml -f docker-compose.prod.yml up php caddy -d --build
+APP_SECRET_NAME="game7-app-secret"
+DATABASE_URL_NAME="game7-dburl"
+
+APP_SECRET=$(aws secretsmanager get-secret-value --secret-id "$APP_SECRET_NAME" --query 'SecretString' --output text)
+DATABASE_URL=$(aws secretsmanager get-secret-value --secret-id "$DATABASE_URL_NAME" --query 'SecretString' --output text)
+
+if [ -z "$APP_SECRET" ] || [ -z "$DATABASE_URL" ]; then
+    echo "Error: Failed to fetch APP_SECRET and/or DATABASE_URL from AWS Secrets Manager."
+    exit 1
+fi
+
+if [ -d 'game7-api' ]; then
+    cd game7-api && git pull
+else
+    git clone git@github.com:Game7-ESGI-2023-PA/game7-api.git
+    cd game7-api || return
+fi
+
+APP_SECRET="$APP_SECRET" \
+DATABASE_URL="$DATABASE_URL" \
+CORS_ALLOW_ORIGIN='*' \
+sudo docker-compose -f docker-compose.yml -f docker-compose.prod.yml up php caddy -d --build
 
 exit_status=$?
 echo "Docker compose exit status: $exit_status"
