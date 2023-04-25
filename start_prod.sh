6e8a459b669c411238c28cd36017ab41e648ef8e
saziz <shan.azizh@gmail.com>
2023-04-25 17:48:39 +0200

    devops: test

--- start_prod.sh
+++ start_prod.sh
@@ -16,6 +16,11 @@ if [ -z "$APP_SECRET" ] || [ -z "$DATABASE_URL" ]; then
     exit 1
 fi
 
+# Write the environment variables to a temporary file
+echo "APP_SECRET=$APP_SECRET" > temp_env
+echo "DATABASE_URL=$DATABASE_URL" >> temp_env
+echo "CORS_ALLOW_ORIGIN='*'" >> temp_env
+
 if [ -d 'game7-api' ]; then
     cd game7-api && git pull
 else
@@ -23,10 +28,13 @@ else
     cd game7-api || return
 fi
 
-sudo APP_SECRET="$APP_SECRET" DATABASE_URL="$DATABASE_URL" CORS_ALLOW_ORIGIN='*' docker compose -f docker-compose.yml -f docker-compose.prod.yml up php caddy -d --build
-
+# Pass the temporary environment file to the Docker Compose command
+sudo docker compose -f docker-compose.yml -f docker-compose.prod.yml --env-file temp_env up php caddy -d --build
 
 exit_status=$?
 echo "Docker compose exit status: $exit_status"
+
+# Cleanup the temporary environment file
+rm temp_env
 
 exit $exit_status
