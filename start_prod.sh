61b0c29e8658c4d4cb5972115ba286de19e4daeb
saziz <shan.azizh@gmail.com>
2023-05-14 16:19:27 +0200

    cd: update secret management

--- start_prod.sh
+++ start_prod.sh
@@ -5,19 +5,29 @@ if [ -z "$(which aws)" ]; then
     exit 1
 fi
 
-APP_SECRET_NAME="game7-app-secret"
-MONGODB_URL_NAME="game7-mongo-url"
-MONGODB_DB_NAME="game7-mongo-dbname"
-JWT_PASSPHRASE_NAME="game7-jwt-passphrase"
+# APP_SECRET_NAME="game7-app-secret"
+# MONGODB_URL_NAME="game7-mongo-url"
+# MONGODB_DB_NAME="game7-mongo-dbname"
+# JWT_PASSPHRASE_NAME="game7-jwt-passphrase"
 
-APP_SECRET=$(aws secretsmanager get-secret-value --secret-id "$APP_SECRET_NAME" --query 'SecretString' --output text)
-MONGODB_URL=$(aws secretsmanager get-secret-value --secret-id "$MONGODB_URL_NAME" --query 'SecretString' --output text)
-MONGODB_DB=$(aws secretsmanager get-secret-value --secret-id "$MONGODB_DB_NAME" --query 'SecretString' --output text)
-JWT_PASSPHRASE=$(aws secretsmanager get-secret-value --secret-id "$JWT_PASSPHRASE_NAME" --query 'SecretString' --output text)
+# APP_SECRET=$(aws secretsmanager get-secret-value --secret-id "$APP_SECRET_NAME" --query 'SecretString' --output text)
+# MONGODB_URL=$(aws secretsmanager get-secret-value --secret-id "$MONGODB_URL_NAME" --query 'SecretString' --output text)
+# MONGODB_DB=$(aws secretsmanager get-secret-value --secret-id "$MONGODB_DB_NAME" --query 'SecretString' --output text)
+# JWT_PASSPHRASE=$(aws secretsmanager get-secret-value --secret-id "$JWT_PASSPHRASE_NAME" --query 'SecretString' --output text)
 
+
+# if [ -z "$APP_SECRET" ] || [ -z "$MONGODB_URL" ] || [ -z "$MONGODB_DB" ] || [ -z "$JWT_PASSPHRASE" ]; then
+#     echo "Error: Failed to fetch APP_SECRET, MONGODB_URL, MONGODB_DB and/or JWT_PASSPHRASE from AWS Secrets Manager."
+#     exit 1
+# fi
+
+APP_SECRET="$1"
+MONGODB_URL="$2"
+MONGODB_DB="$3"
+JWT_PASSPHRASE="$4"
 
 if [ -z "$APP_SECRET" ] || [ -z "$MONGODB_URL" ] || [ -z "$MONGODB_DB" ] || [ -z "$JWT_PASSPHRASE" ]; then
-    echo "Error: Failed to fetch APP_SECRET, MONGODB_URL, MONGODB_DB and/or JWT_PASSPHRASE from AWS Secrets Manager."
+    echo "Error: Missing parameters. The script requires APP_SECRET, MONGODB_URL, MONGODB_DB and JWT_PASSPHRASE as named parameters."
     exit 1
 fi
 
