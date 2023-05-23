c55b25b6fe2ea1169f9e2d0b34c8be5a62ec0491
saziz <shan.azizh@gmail.com>
2023-05-23 10:28:19 +0200

    feat: game lobby join logic

--- src/Document/GameLobby.php
+++ src/Document/GameLobby.php
@@ -6,11 +6,18 @@ use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
+use App\Exception\GameLobbyException;
 use App\Repository\GameLobbyRepository;
 use App\State\GameLobby\GameLobbyCreationProcessor;
+use App\State\GameLobby\GameLobbyJoinProcessor;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Serializer\Annotation\Groups;
+
+// TODO: change status (only master) -> trigger le moteur de jeux
+// TODO: start the game (only master) -> send information to game dispatcher
 
 #[ODM\Document(repositoryClass: GameLobbyRepository::class)]
 #[ApiResource(
@@ -18,6 +25,12 @@ use Symfony\Component\Serializer\Annotation\Groups;
         new GetCollection(),
         new Post(
             processor: GameLobbyCreationProcessor::class
+        ),
+        new Put(
+            uriTemplate: '/game_lobbies/{id}/join',
+            exceptionToStatus: [GameLobbyException::class => 400],
+            denormalizationContext: ['groups' => ['gameLobby:join']],
+            processor: GameLobbyJoinProcessor::class,
         )
     ],
     normalizationContext: ['groups' => ['gameLobby:read']],
@@ -44,6 +57,18 @@ class GameLobby
     )]
     #[Groups(['gameLobby:write', 'gameLobby:read'])]
     private ?Game $game = null;
+
+    #[ODM\ReferenceMany(targetDocument: User::class)]
+    #[ApiProperty(
+        example: '["/api/users/{userId}", "/api/users/{userId}"]',
+    )]
+    #[Groups(['gameLobby:read'])]
+    private ?ArrayCollection $players = null;
+
+    public function __construct()
+    {
+        $this->players = new ArrayCollection();
+    }
 
     public function getId(): ?string
     {
@@ -79,4 +104,26 @@ class GameLobby
     {
         $this->game = $game;
     }
+
+    public function getPlayers(): ArrayCollection
+    {
+        return $this->players;
+    }
+
+    public function addPlayers(User $user): self
+    {
+        if (!$this->players->contains($user)) {
+            $this->players[] = $user;
+        }
+
+        return $this;
+    }
+
+    public function removePlayers(User $user): self
+    {
+        $this->players->removeElement($user);
+
+        return $this;
+    }
+
 }
---
+++ src/Exception/GameLobbyException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Exception;
+
+class GameLobbyException extends \Exception
+{
+
+}
